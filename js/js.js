71d05a85a5d411cea2ee253e9af889230e123d13
Nikita <88242275+Sabbanya@users.noreply.github.com>
2022-12-21 13:44:17 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,7 @@
+let modalwindow = document.getElementById("modal");
+function open_window() {
+  modalwindow.classList.add("active");
+}
+function close_window() {
+  modalwindow.classList.remove("active");
+}
